957fa626fe1a682944def0b38b759dac0a529042
Zhangdwd529 <1348100291@qq.com>
2022-08-22 10:10:28 +0800

    test

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+    int n;
+    cin >> n;
+
+    cout << "hello" << endl;
+    cout << " world " << endl;
+    cout << n << endl;
+
+    return 0;
+    
+}
